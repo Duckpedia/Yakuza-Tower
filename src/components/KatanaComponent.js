f8d6a46b6c71e1224ff51aec471a5f7b644ee72b
Duckpedia <nika.hocevar67@gmail.com>
2026-01-11 21:38:11 +0100

    fixed katana priblizno

--- src/components/KatanaComponent.js
+++ src/components/KatanaComponent.js
@@ -16,49 +16,52 @@ export class KatanaComponent {
     }
 
     startAttack() {
+        this.player.skeleton.playAnimation("Swing");
         this.isAttacking = true;
-        setTimeout(() => {
-            this.isAttacking = false;
-            if(this.holder === this.player){
-                this.player.skeleton.playAnimation(0); //Return to idle animation
-            }
-        }, 500); // 0.5 sekund
     }
 
-    update() {
-        if (!this.isAttacking) return;
+    update(){
+        if(!this.isAttacking) return;
 
-        // Get world position of handle
         const worldPos = this.transform.final_position;
 
-        // Calculate the world position of tip
         const tipWorld = glm.vec3.create();
         glm.vec3.transformMat4(tipWorld, this.tipOffset, this.transform.final);
 
-        // Handle to tip
         const direction = glm.vec3.create();
         glm.vec3.subtract(direction, tipWorld, worldPos);
         glm.vec3.normalize(direction, direction);
 
-        // Calculate end position for raycast
         const endPos = glm.vec3.create();
         glm.vec3.scaleAndAdd(endPos, worldPos, direction, this.attackRange);
 
         if(this.holder === this.player){
-            console.log("Player katana attack...");
-            const hitEnemy = World.physics.raycast(worldPos, endPos, World.scene, Layers.ENEMY);
-            if (hitEnemy) {
+            const hitEnemy = World.physics.raycast(
+                worldPos,
+                endPos,
+                World.scene,
+                Layers.ENEMY
+            );
+
+            if(hitEnemy){
                 hitEnemy.entity._parent.hidden = true;
-                console.log("Katana hit enemy");
-                return;
+                this.isAttacking = false;
             }
-        }
-        const hitPlayer = World.physics.raycast(worldPos, endPos, World.scene, Layers.PLAYER);
-        if (hitPlayer) {
-            console.log("Katana hit player");
-            location.reload();
-            this.isAttacking = false;
+
             return;
         }
+
+        const hitPlayer = World.physics.raycast(
+            worldPos,
+            endPos,
+            World.scene,
+            Layers.PLAYER
+        );
+
+        if(hitPlayer){
+            World.loadStage = "tutorial";
+            this.isAttacking = false;
+        }
     }
+
 }
